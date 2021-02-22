ceb693513c086ae349fedf261d433c969b7fda18
Siyovush Tuychiev <siyovusht@gmail.com>
2021-02-22 18:38:29 +0300

    UI updated

--- flack/routes.py
+++ flack/routes.py
@@ -4,7 +4,24 @@ from flask.helpers import url_for
 from werkzeug.utils import redirect
 from flack import app
 
+
+class Channel:
+    def __init__(self, name):
+        self.name = name
+        self.users = []
+        self.messages = []
+    
+    def __repr__(self):
+        return self.name
+    
+
+
+channel = Channel('General')
+channel.messages = ['aaa' for _ in range(20)]
+channels = ['clubhouse', 'semiplanet']
 users = set()
+
+
 def login_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -14,16 +31,17 @@ def login_required(f):
             return f(*args, **kwargs)
     return wrapper
 
+
 @app.route('/')
 @login_required
 def home():
-    username = session.get("USERNAME", 'world')
-    return render_template("home.html", username=username)
+    username = session.get("USERNAME")
+    return render_template("home.html", username=username, channel=channel, channels=channels)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = ""
+    error = None
     if session.get('USERNAME'):
         return redirect(url_for('home'))
     
